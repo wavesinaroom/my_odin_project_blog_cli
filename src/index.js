ffff064699328eee9e24fe46f4a6a7cd446a5fde
wavesinaroom <jaureguij@javeriana.edu.co>
2024-01-09 12:41:31 -0500

    [Feature] Project setup

---
+++ src/index.js
@@ -0,0 +1,14 @@
+import express from 'express';
+import mongoose from 'mongoose';
+
+const app = express();
+
+app.listen(3000, ()=>{
+  console.log("Example app listening on port 3000!");
+});
+
+main().catch(err => console.log(err));
+
+async function main(){
+  await mongoose.connect(process.env.MONGODB_URI)
+}
