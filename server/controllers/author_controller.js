c107ad9e89d703269742e263060e0eaa68986b11
wavesinaroom <jaureguij@javeriana.edu.co>
2024-01-11 15:57:12 -0500

    [Task] Add remaining routes and controller functions

--- server/controllers/author_controller.js
+++ server/controllers/author_controller.js
@@ -1,7 +1,63 @@
 import asyncHandler from "express-async-handler";
 
 const author_entry_create_get = asyncHandler(async(req,res,next)=>{
-  res.send("NOT IMPLEMENTED: Author entry create");
+  res.send('NOT IMPLEMENTED: Author entry create GET');
 });
 
-export {author_entry_create_get};
+const author_entry_create_post = asyncHandler(async(req,res,next)=>{
+  res.send('NOT IMPLEMENTED: Author entry  create POST');
+});
+
+const author_entries = asyncHandler(async(req,res,next)=>{
+  res.send('NOT IMPLEMENTED: Author entry list');
+})
+
+const author_entry_edit_get = asyncHandler(async(req,res,next)=>{
+  res.send('NOT IMPLEMENTED: Author entry edit GET');
+});
+
+const author_entry_edit_put = asyncHandler(async(req,res,next)=>{
+  res.send('NOT IMPLEMENTED: Author entry edit POST');
+});
+
+const author_entry_delete_get = asyncHandler(async(req,res,next)=>{
+  res.send('NOT IMPLEMENTED: Author entry delete GET');
+});
+
+const author_entry_delete_post = asyncHandler(async(req,res,next)=>{
+  res.send('NOT IMPLEMENTED: Author entry delete POST')
+});
+
+const author_entry_publish_get = asyncHandler(async(req,res,next)=>{
+  res.send('NOT IMPLEMENTED: Author entry publish GET');
+});
+
+const author_entry_publish_post = asyncHandler(async(req,res,next)=>{
+  res.send('NOT IMPLEMENTED: Author entry publish POST');
+});
+
+const author_entry_comments = asyncHandler(async(req,res,next)=>{
+  res.send('NOT IMPLETEMENTED: Author entry comments GET');
+});
+
+const author_entry_comment_delete_get = asyncHandler(async(req,res,next)=>{
+  res.send('NOT IMPLEMENTED: Author entry comments GET');
+});
+
+const author_entry_comment_delete_post = asyncHandler(async(req,res,next)=>{
+  res.send('NOT IMPLEMENTED: Author entry comments POST');
+});
+
+
+export {author_entry_create_get,
+        author_entry_create_post,
+        author_entries,
+        author_entry_edit_get,
+        author_entry_edit_put,
+        author_entry_delete_get,
+        author_entry_delete_post,
+        author_entry_publish_get,
+        author_entry_publish_post,
+        author_entry_comments,
+        author_entry_comment_delete_get,
+        author_entry_comment_delete_post};
--- server/routes/author.js
+++ server/routes/author.js
@@ -3,7 +3,18 @@ import * as authorController from '../controllers/author_controller';
 
 const router = express.Router();
 
-router.get('/new_entry', authorController.author_entry_create_get);
+router.get('/entry/create', authorController.author_entry_create_get);
+router.post('/entry/create', authorController.author_entry_create_post);
+router.get('/entries', authorController.author_entries);
+router.get('/entry/:title/edit', authorController.author_entry_edit_get);
+router.put('/entry/:title/edit', authorController.author_entry_edit_put);
+router.get('/entry/:title/delete', authorController.author_entry_delete_get);
+router.post('/entry/:title/delete', authorController.author_entry_delete_post);
+router.get('/entry/:title/publish', authorController.author_entry_publish_get);
+router.post('/entry/:title/publish', authorController.author_entry_publish_post);
+router.get('/entry/:title/comments', authorController.author_entry_comments);
+router.get('/entry/:title/comment/:id', authorController.author_entry_comment_delete_get);
+router.post('/entry/:title/comment/:id', authorController.author_entry_comment_delete_post);
 
 module.exports = router;
 
