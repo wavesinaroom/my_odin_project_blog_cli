61e68d23f898757cf0bbb10e87421e8d14ac0175
wavesinaroom <jaureguij@javeriana.edu.co>
2024-01-11 10:28:06 -0500

    [Task] Rename model and complete its definition. It was half way through

--- server/models/entry.js
+++ server/models/entry.js
@@ -2,10 +2,12 @@ import mongoose from "mongoose";
 
 const Schema = mongoose.Schema;
 
-const Post = new Schema({
+const EntrySchema = new Schema({
   title: String,
   date: Date,
   text: String,
   is_published: Boolean,
   comments: [{type: Schema.Types.ObjectId, ref: "Comment"}] 
 })
+
+const Entry = mongoose.model('Entry', EntrySchema);
