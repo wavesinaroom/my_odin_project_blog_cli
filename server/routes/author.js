4f5cdf86388892f5daf980fc865e4f33c42c8e81
wavesinaroom <jaureguij@javeriana.edu.co>
2024-01-16 11:46:46 -0500

    [Task] Edit controller action names

--- server/routes/author.js
+++ server/routes/author.js
@@ -3,22 +3,22 @@ import * as authorController from '../controllers/author_controller';
 
 const router = express.Router();
 
-router.get('/',authorController.author_main_get);
-router.get('/entry/create', authorController.author_entry_create_get);
-router.post('/entry/create', authorController.author_entry_create_post);
-router.get('/entries', authorController.author_entries);
-router.get('/entry/:title/edit', authorController.author_entry_edit_get);
-router.put('/entry/:id/edit', authorController.author_entry_edit_put);
-router.get('/entry/:title/delete', authorController.author_entry_delete_get);
-router.post('/entry/:title/delete', authorController.author_entry_delete_post);
-router.get('/entry/publish', authorController.author_entry_publish_get);
-router.put('/entry/:id/publish', authorController.author_entry_publish_put);
-router.get('/entry/unpublish', authorController.author_entry_unpublish_get);
-router.put('/entry/:id/unpublish', authorController.author_entry_unpublish_put);
-router.get('/entry/:title/comments', authorController.author_entry_comments);
-router.get('/entry/:title/comment/:id', authorController.author_entry_comment_delete_get);
-router.post('/entry/:title/comment/:id', authorController.author_entry_comment_delete_post);
+router.get('/',authorController.main_get);
+router.get('/entry/create', authorController.entry_create_get);
+router.post('/entry/create', authorController.entry_create_post);
+router.get('/entries', authorController.entries);
+router.get('/entry/:title/edit', authorController.entry_edit_get);
+router.put('/entry/:id/edit', authorController.entry_edit_put);
+router.get('/entry/:title/delete', authorController.entry_delete_get);
+router.post('/entry/:title/delete', authorController.entry_delete_post);
+router.get('/entry/publish', authorController.entry_publish_get);
+router.put('/entry/:id/publish', authorController.entry_publish_put);
+router.get('/entry/unpublish', authorController.entry_unpublish_get);
+router.put('/entry/:id/unpublish', authorController.entry_unpublish_put);
+router.get('/entry/:title/comments', authorController.entry_comments);
+router.get('/entry/:title/comment/:id', authorController.entry_comment_delete_get);
+router.post('/entry/:title/comment/:id', authorController.entry_comment_delete_post);
 
-module.exports = router;
+export default router;
 
 
