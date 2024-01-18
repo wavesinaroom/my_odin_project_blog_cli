54a5997fe6e5852a1bece875ea7f7a80e2b004aa
wavesinaroom <jaureguij@javeriana.edu.co>
2024-01-18 17:16:58 -0500

    [Fix] Import Strategy from passport-local

--- server/src/index.js
+++ server/src/index.js
@@ -33,7 +33,7 @@ app.use('/reader', readerRouter);
 import session from 'express-session';
 import passport from 'passport';
 import User from '../models/user.js';
-const LocalStrategy = require('passport-local').LocalStrategy;
+const LocalStrategy = require('passport-local').Strategy;
 
 app.use(session({secret: "cats", resave: false, saveUninitialized: true}));
 app.use(passport.initialize());
