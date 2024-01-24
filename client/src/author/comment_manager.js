ba21577ed30661f36de6acb4ea8523d6f1756a16
wavesinaroom <jaureguij@javeriana.edu.co>
2024-01-24 17:34:58 -0500

    [Feature] Comment management

---
+++ client/src/author/comment_manager.js
@@ -0,0 +1,41 @@
+import { select, confirm } from "@inquirer/prompts";
+
+const entries_list = async()=>{
+  fetch('http://localhost:3000/author/entries')
+    .then((data)=>{
+      return data.json();
+    })
+    .then(async(entries)=>{
+      const options = [];
+      entries.list.forEach((e)=>{
+        options.push({name: e.title, value: e.title});
+      });
+      const answer = await select({message: 'Select article', choices: options})
+      comments_list(answer);
+    });
+};
+
+const comments_list = async(entry)=>{
+  const endpoint = `http://localhost:3000/author/entry/${entry}/comments`
+  fetch(endpoint)
+    .then((data)=>{
+      return data.json();
+    })
+    .then(async(comments)=>{
+      const options = [];
+      comments.list.forEach((e)=>{
+        options.push({name: e.text, value: {title: entry, id:e._id}})
+      });
+      const answer = {comment: await select({message: 'Please select a comment to delete', choices:options}), 
+                      confirmation: await confirm({message: 'Are you sure you want to delete this comment?'})}
+      if(remove.confirmation)
+        remove(answer.comment);
+    });
+}
+
+const remove = async(comment)=>{
+  const endpoint = `http://localhost:3000/author/${comment.title}/comment/${comment.id}`;
+  fetch(endpoint, {method: 'PUT'});
+};
+
+export { entries_list }
