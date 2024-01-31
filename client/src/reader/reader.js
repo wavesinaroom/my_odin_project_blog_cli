c1c6a363e7d2868c429cb0c81b0a6a2fb3ef8da7
wavesinaroom <jaureguij@javeriana.edu.co>
2024-01-31 15:11:43 -0500

    [Task] Hook up reader modules to menu

--- client/src/reader/reader.js
+++ client/src/reader/reader.js
@@ -1,6 +1,19 @@
+import { select } from '@inquirer/prompts';
 import viewer from './viewer.js';
 import comment_manager from './comment_manager.js';
 
 export default async ()=>{
-  comment_manager();
+  const answer = await select({message: "Do you want to read or comment an article", 
+    choices:[
+      {name: 'Read', value: 'read'},
+      {name: 'Comment', value: 'comment'}
+    ]
+  });
+
+  if(answer === 'read')
+    viewer();
+  else if(answer === 'comment')
+    comment_manager();
+  else
+    console.error('Unknown option');
 };
